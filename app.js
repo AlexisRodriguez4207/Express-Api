0cc17f37cfa3f03e2169e233ef46672618e476bb
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2022-04-30 02:02:29 -0700

    Agregando creacion de un Endpoint Put

--- app.js
+++ app.js
@@ -24,3 +24,20 @@ app.get('/v1/explorers', (req,res) => {
     const explorers = [explorer1,explorer2,explorer3,explorer4]
     res.status(200).json(explorers)
 })
+
+// GET creacion de un endpoint que te regrese un explorer mediando un ID
+app.get('/v1/explorers/:id', (req,res) =>{
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id:1, name:"Carlo"}
+    res.status(200).json(explorer)
+})
+
+// POST Crea un endpoint que se encargue de crear un explorer
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    // Parámetros de un cliente
+    const requestBody = req.body
+    res.status(201).json({messegae: "Created"})
+})
+
